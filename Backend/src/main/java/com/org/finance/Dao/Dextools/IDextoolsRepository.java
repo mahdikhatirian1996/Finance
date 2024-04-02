e81b8d2ab9d3f8d4049273565dab9207890bfd6f
Mahdi-Khatirian <mehdikhatir1375@gmail.com>
2024-04-02 05:11:11 +0330

    Build NewPairDto With Spring-Data Repositories In Spring Application

--- Backend/src/main/java/com/org/finance/Dao/Dextools/IDextoolsRepository.java
+++ Backend/src/main/java/com/org/finance/Dao/Dextools/IDextoolsRepository.java
@@ -1,8 +1,18 @@
 package com.org.finance.Dao.Dextools;
 
 import com.org.finance.Model.Main.DextoolsInfo;
+import com.org.finance.Model.Main.HoneypotInfo;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface IDextoolsRepository extends JpaRepository<DextoolsInfo, String> {
+import java.util.List;
+
+public interface IDextoolsRepository extends
+        JpaRepository<DextoolsInfo, String>,
+        PagingAndSortingRepository<DextoolsInfo, String> {
     DextoolsInfo findByContractAddress(String contractAddress);
+
+    List<DextoolsInfo> findAllByContractAddress(String contractAddress, Pageable pageable);
+
 }
