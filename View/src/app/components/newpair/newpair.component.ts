f7946494d8680e3ba19dfaba4a960cfef8c0c802
Mahdi-Khatirian <mehdikhatir1375@gmail.com>
2024-04-06 08:17:48 +0330

    Design And Develop Main Live-Newpair Grid Page

--- View/src/app/components/newpair/newpair.component.ts
+++ View/src/app/components/newpair/newpair.component.ts
@@ -1,5 +1,5 @@
-import {Component, OnInit} from '@angular/core';
-import {Tutorial} from '../../models/tutorial.model';
+import {interval, Subscription} from 'rxjs';
+import {Component, Directive, OnInit, OnDestroy} from '@angular/core';
 import {NewPairModel} from "../../models/newpair/newpair.model";
 import {NewPairService} from "../../services/newpair/newpair.service";
 
@@ -7,33 +7,79 @@ import {NewPairService} from "../../services/newpair/newpair.service";
   selector: 'app-newpair-list',
   templateUrl: './newpair.component.html'
 })
-export class NewpairComponent implements OnInit {
-  newPairModels?: NewPairModel[];
-  currentTutorial: Tutorial = {};
-  currentIndex = -1;
-  title = '';
+export class NewpairComponent implements OnInit, OnDestroy {
+  // --- Grid Params ---
+  newPairModels: NewPairModel[] = [];
+  hasInterval: boolean = false;
+  subscription: Subscription = new Subscription();
+  // --- Page Params ---
+  currentPage: number = 1;
+  itemsPerPage: number = 10;
+  totalItems: number = 0;
+  totalPages: number = Math.ceil(this.totalItems / this.itemsPerPage);
 
   constructor(private newPairService: NewPairService) {
   }
 
   ngOnInit(): void {
+    this.setInterval();
     this.getAll();
   }
 
+  setInterval() {
+    const intervalOperator = interval(5000);
+    this.subscription = intervalOperator.subscribe(() => {
+      if (this.hasInterval) {
+        this.refreshList();
+      }
+    })
+  }
+
   getAll(): void {
-    this.newPairService.getAll().subscribe({
+    this.newPairService.getPaginatedData(this.currentPage - 1, this.itemsPerPage).subscribe({
       next: (data) => {
-        this.newPairModels = data;
-        console.log(data);
+        this.newPairModels = data.list;
+        this.setTimestamp();
+        this.totalItems = data.count;
+        this.totalPages = Math.ceil(data.totalItems / this.itemsPerPage);
       },
       error: (e) => console.error(e)
     });
   }
 
-  refreshList(): void {
+  onPageChange(page: number): void {
+    this.currentPage = page;
     this.getAll();
-    this.currentTutorial = {};
-    this.currentIndex = -1;
+  }
+
+  refreshList(): void {
+    this.currentPage = 0;
+    this.getAll();
+  }
+
+  setTimestamp(): void {
+    for (let i = 0; i < this.newPairModels.length; i++) {
+      if (!(this.newPairModels) || this.newPairModels[i].createdDateDI !== null) {
+        // @ts-ignore
+        this.newPairModels[i].createdDateDITimestamp = new Date(this.newPairModels[i].createdDateDI).toISOString();
+      }
+      if (!(this.newPairModels) || this.newPairModels[i].createdDateHI !== null) {
+        // @ts-ignore
+        this.newPairModels[i].createdDateHITimestamp = new Date(this.newPairModels[i].createdDateHI).toISOString();
+      }
+    }
+  }
+
+  changeHasInterval() {
+    if (this.hasInterval) {
+      this.hasInterval = false;
+    } else if (!this.hasInterval) {
+      this.hasInterval = true;
+    }
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
