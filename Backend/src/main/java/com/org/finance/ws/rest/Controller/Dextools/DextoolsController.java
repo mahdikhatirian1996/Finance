ac7f589442efa6a0de5e134372436f5a9986d5cf
Mahdi-Khatirian <mehdikhatir1375@gmail.com>
2024-04-13 00:46:27 +0330

    Set Rest-Api To Save Solana And Base

--- Backend/src/main/java/com/org/finance/ws/rest/Controller/Dextools/DextoolsController.java
+++ Backend/src/main/java/com/org/finance/ws/rest/Controller/Dextools/DextoolsController.java
@@ -2,6 +2,7 @@ package com.org.finance.ws.rest.Controller.Dextools;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.org.finance.Dto.Dextools.DextoolsInfoDto;
+import com.org.finance.Model.Enum.CurrencyType;
 import com.org.finance.Model.Main.DextoolsInfo;
 import com.org.finance.Service.Dextools.IDextoolsService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,7 +20,7 @@ public class DextoolsController {
 
     @GetMapping("/saveData/{params}")
     public void saveData(
-        @PathVariable("params") String params
+            @PathVariable("params") String params
     ) throws Exception {
         try {
             DextoolsInfo savedObject = iDextoolsService.save(
@@ -40,13 +41,37 @@ public class DextoolsController {
 
     @ResponseBody
     @PostMapping("/saveData/solana")
-    public void saveSolanaData(@RequestBody DextoolsInfoDto dto){
-        System.out.println(dto);
+    public void saveSolanaData(@RequestBody DextoolsInfoDto dto) {
+        try {
+            // Exception Occur On : Cannot invoke "java.sql.Timestamp.getDate()" because "objectTs" is null
+            DextoolsInfo savedObject = iDextoolsService.saveWithoutHoneypot(
+                    iDextoolsService.mapDtoToEntity(dto, CurrencyType.SOLANA)
+            );
+            if (savedObject.getContractAddress() != null) {
+                System.out.println("Save At : " + new Timestamp(System.currentTimeMillis()));
+            } else {
+                System.out.println("Object Isn't Valid Cause : " + savedObject.getErrorType().getEnglishTitle());
+            }
+        } catch (Exception e) {
+            System.out.println("Exception Occur On : " + e.getMessage());
+        }
     }
+
     @ResponseBody
     @PostMapping("/saveData/base")
-    public void saveBaseData(@RequestBody DextoolsInfoDto dto){
-        System.out.println(dto);
+    public void saveBaseData(@RequestBody DextoolsInfoDto dto) {
+        try {
+            DextoolsInfo savedObject = iDextoolsService.saveWithoutHoneypot(
+                    iDextoolsService.mapDtoToEntity(dto, CurrencyType.BASE)
+            );
+            if (savedObject.getContractAddress() != null) {
+                System.out.println("Save At : " + new Timestamp(System.currentTimeMillis()));
+            } else {
+                System.out.println("Object Isn't Valid Cause : " + savedObject.getErrorType().getEnglishTitle());
+            }
+        } catch (Exception e) {
+            System.out.println("Exception Occur On : " + e.getMessage());
+        }
     }
 
 }
